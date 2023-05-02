4e8ad3a0b3ee990b6a0ba4fbaa1322c6ced773cc
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-02 17:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log("The " + this._species + " makes a sound.");
+  }
+}
 
-// Do not change the code below this line
+class Cat extends Animal {
+  purr() {
+    console.log("The Siamese makes a sound");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("The Golden Retriever makes a sound");
+  }
+}
+
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
